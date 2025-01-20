183f214f3c44979c6b4b988f9d63da57482e032c
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-20 11:40:31 -0700

    Proyecto con archivos base

---
+++ src/app/pages/contact/contact-page.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'contact-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class ContactPageComponent {
+
+}
---
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'pricing-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './pricing-page.component.html',
+  styleUrl: './pricing-page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export default class PricingPageComponent {
+
+}
