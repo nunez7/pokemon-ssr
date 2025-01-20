1e971e4c747a5d88fcbbc7be72a6ebaec5e38487
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-20 12:03:12 -0700

    Habilitando NavBar

---
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './navbar.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NavbarComponent {
+
+}
