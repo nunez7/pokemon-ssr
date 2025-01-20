72cedbf410355cb3012fff16d820dc3705a55810
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-20 16:14:45 -0700

    Agregando un skeleton

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,13 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from '../../pokemons/components/pokemon-list/pokemon-list.component';
+import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
 
 @Component({
   selector: 'pokemons-page',
-  imports: [PokemonListComponent],
+  imports: [PokemonListComponent, PokemonListSkeletonComponent],
   templateUrl: './pokemons-page.component.html',
   styleUrl: './pokemons-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export default class PokemonsPageComponent {
+export default class PokemonsPageComponent implements OnInit{
+
+  //public isLoading = signal(true);
+
+  ngOnInit(): void {
+   /* setTimeout(() => {
+      this.isLoading.set(false);
+    }, 1500);*/
+  }
 
 }
