c9db1aed21f57d39fafdce51717af506ef4c0ded
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-20 14:43:33 -0700

    PLATFORM_ID Cliente y servidor
    - Se agrega routerlink tambien

--- src/app/pages/pricing/pricing-page.component.ts
+++ src/app/pages/pricing/pricing-page.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
@@ -13,9 +14,15 @@ export default class PricingPageComponent implements OnInit{
 
   private readonly title = inject(Title);
   private readonly meta = inject(Meta);
+  private readonly platform = inject(PLATFORM_ID);
 
   
   ngOnInit(): void {
+    // if (isPlatformBrowser(this.platform)) {
+    //   document.title = 'Pricing Page';
+    // }
+
+    // console.log({ hola: 'mundo' });
      this.title.setTitle('Pricing Page');
      this.meta.updateTag({ name: 'description', content: 'Pricing Page Description' });
      this.meta.updateTag({ name: 'og:title', content: 'Pricing Page Description' });
