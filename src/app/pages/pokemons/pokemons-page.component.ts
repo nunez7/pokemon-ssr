0186c9a0f159bcb1e9fc61ea59e04a474e511b78
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-20 16:44:34 -0700

    servicio http

--- src/app/pages/pokemons/pokemons-page.component.ts
+++ src/app/pages/pokemons/pokemons-page.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import { ApplicationRef, ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from '../../pokemons/components/pokemon-list/pokemon-list.component';
+import { PokemonsService } from '../../pokemons/services/pokemons.service';
 import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
 
 @Component({
@@ -11,12 +12,29 @@ import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon
 })
 export default class PokemonsPageComponent implements OnInit{
 
-  //public isLoading = signal(true);
+  public isLoading = signal(true);
+  private pokemonsService = inject(PokemonsService);
+  //private appRef = inject(ApplicationRef);
+
+  /*private $appState = this.appRef.isStable.subscribe((isStable) => {
+    console.log({ isStable });
+  });*/
 
   ngOnInit(): void {
-   /* setTimeout(() => {
+    this.loadPokemons();
+    /*setTimeout(() => {
       this.isLoading.set(false);
     }, 1500);*/
   }
+
+  public loadPokemons(page = 0) {
+    this.pokemonsService.loadPage(page).subscribe((pokemons) => {
+      console.log({ pokemons });
+    });
+  }
+
+  // ngOnDestroy(): void {
+  //   this.$appState.unsubscribe();
+  // }
 
 }
