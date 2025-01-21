9893d7a1b1554044fb9871640dadede68b23ac1c
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-21 12:45:51 -0700

    Probando AppRoutes

---
+++ src/app/app.routes.spec.ts
@@ -0,0 +1,55 @@
+import { TestBed } from "@angular/core/testing";
+import { provideRouter, Router } from "@angular/router";
+import { routes } from "./app.routes";
+import { Location } from '@angular/common';
+
+describe('App Routes', () => {
+    let router: Router;
+    let location: Location;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+          providers: [provideRouter(routes)],
+        });
+    
+        router = TestBed.inject(Router);
+        location = TestBed.inject(Location);
+      });
+
+    it('should navigate to "about" redirects to "/about" ', async () => {
+        await router.navigate(['about']);
+    
+        expect(location.path()).toBe('/about');
+      });
+
+      it('should navigate to "pokemons/page/1" redirects to "/pokemons/page/1" ', async () => {
+        await router.navigate(['unknown-page']);
+    
+        // console.log(location.path());
+    
+        expect(location.path()).toBe('/about');
+      });
+
+      it('should navigate to "pokemons/page/1" redirects to "/pokemons/page/1" ', async () => {
+        await router.navigate(['unknown-page']);
+    
+        // console.log(location.path());
+    
+        expect(location.path()).toBe('/about');
+      });
+
+      it('should load the proper component', async () => {
+        const aboutRoute = routes.find((route) => route.path === 'about')!;
+        expect(aboutRoute).toBeDefined();
+        const aboutComponent = (await aboutRoute.loadComponent!()) as any;
+        expect(aboutComponent.default.name).toBe('AboutPageComponent');
+    
+        const pokemonPageRoute = routes.find(
+          (route) => route.path === 'pokemons/page/:page'
+        )!;
+        expect(pokemonPageRoute).toBeDefined();
+        const pokemonPage = (await pokemonPageRoute.loadComponent!()) as any;
+        expect(pokemonPage.default.name).toBe('PokemonsPageComponent');
+      });
+
+});
