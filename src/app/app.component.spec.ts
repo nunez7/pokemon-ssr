ce6e1044762d001f64227a848ba34e6fdc571208
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-21 12:29:17 -0700

    Componente Mock en pruebas

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,17 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideRouter } from '@angular/router';
+import { Component } from '@angular/core';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 describe('AppComponent', () => {
   let fixture : ComponentFixture<AppComponent>;
   let app: AppComponent;
   let compiled: HTMLDivElement;
 
+  @Component({
+    selector: 'app-navbar',
+    standalone: true,
+    template: `<h1>Hola Mundo</h1>`,
+  })
+  class NavbarComponentMock {}
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
       providers: [provideRouter([])],
-    }).compileComponents();
+    })
+    .overrideComponent(AppComponent, {
+      add: {
+        imports: [NavbarComponentMock],
+      },
+      remove: { 
+        imports: [NavbarComponent],
+      },  
+    })
+    .compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
     compiled = fixture.nativeElement;
